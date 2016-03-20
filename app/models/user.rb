717979a1b6feed8a864121accbf9a02a1f20eb89
VolkovMikhail350504 <antonn.lida@mail.ru>
2016-03-20 03:16:17 +0300

    set default role for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
   has_many :orders
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  before_create :set_default_role
+  private
+  def set_default_role
+    self.role ||= Role.find_by_name('forum_user')
+  end
 end
